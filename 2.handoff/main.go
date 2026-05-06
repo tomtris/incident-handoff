b798af816ec05bb4050ae662b08050979eee1b93
Quoc Tri Do <quoctrido.jb@gmail.com>
2026-05-06 19:43:24 +0200

    implement graceful shutdown

--- 2.handoff/main.go
+++ 2.handoff/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
-
 	memoryStore := MemoryStore{incidents: make(map[string]Incident)}
 	incHandler := IncidentHandler{Store: &memoryStore}
 	router := getRouter(incHandler)
@@ -15,5 +20,20 @@ func main() {
 	srv.Addr = ":8080"
 	srv.Handler = router
 
-	log.Fatal(srv.ListenAndServe())
+	go func() {
+		slog.Info("server running")
+		err := srv.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	slog.Info("server shut down in 10 sec")
+	srv.Shutdown(ctx)
+	slog.Info("server shut down gracefully")
 }
